e51f2c93ac1595c7bafb4f2a8bedc9e45721109b
CalebJ31 <wilson.1112@wright.edu>
2025-11-01 20:28:30 -0400

    adding methods

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,17 @@
 /**
  * HashTable.cpp
  */
+#include "HashTable.h"
+HashTable::HashTable(size_t initCapacity = 8){};
+bool HashTable::insert(std::string key, size_t value){};
+bool HashTable::remove(std::string key){};
+bool HashTable::contains(const string& key) const{};
+std::optional<int> HashTable::get(const string& key) const{};
+int& HashTable::operator[](const string& key){};
+std::vector<string> HashTable::keys() const{};
+double HashTable::alpha() const{};
+size_t HashTable::capacity() const{};
+size_t HashTable::size() const{};
+
+
+
--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,27 @@
 /**
  * HashTable.h
+ *
  */
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <string>
+#include <vector>
+#include <optional>
+#include <iostream>
+
+class HashTable {
+
+public:
+    HashTable(size_t initCapacity = 8);
+    bool insert(std::string key, size_t value);
+    bool remove(std::string key);
+    bool contains(const std::string& key) const;
+    std::optional<size_t> get(const std::string& key) const;
+    size_t& operator[](const std::string& key);
+    std::vector<std::string> keys() const;
+    double alpha() const;
+    size_t capacity() const;
+    size_t size() const;
+};
+#endif
